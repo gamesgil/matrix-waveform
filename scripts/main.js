75669bff3d0ad40bad28bcbbd78d4bac6094bb86
Gil Steiner <Gil.Steiner@birdaero.com>
2018-12-30 14:04:44 +0200

    - added interaction - pretext - fonts

--- scripts/main.js
+++ scripts/main.js
@@ -4,66 +4,88 @@ const RESOLUTION = Math.floor(WIDTH / 8);
 const MAX_RADIUS = 8;
 const CENTER = Math.floor(RESOLUTION / 2);
 const PAD = 24;
-let stop = false;
+let clicked = false;
 let color = [50, 50, 240];
 let vecR = 0;
 let vecG = 0;
 let vecB = 0;
+let font;
 
 function preload() {
     soundFormats('mp3', 'ogg');
     // mySound = loadSound('assets/Phillip_Gross_-_01_-_Casablanca.mp3');
     //mySound = loadSound('assets/Phillip_Gross_-_03_-_Tumbona.mp3');
     mySound = loadSound('assets/aderin-the-response.mp3');
-    mySound.setVolume(1);
+
+    font = loadFont('assets/mexcellent 3d.ttf');
+
   }
 
 function setup() {
     var cnv = createCanvas(windowWidth, windowHeight);
     // cnv.setStyle('display', 'block');
-    mySound.setVolume(0.5);
-    mySound.play();
     
-    fft = new p5.FFT(0.8, WIDTH);
-
     background('black')
 
     //    refresh()
     strokeWeight(3)
     stroke('gray')
 
-   setInterval(_ => {
-       const colorR = Math.floor((1 + Math.sin(vecR)) * 128);
-       const colorG = Math.floor((1 + Math.sin(vecG)) * 128);
-       const colorB = Math.floor((1 + Math.sin(vecB)) * 128);
-       
-       vecR += 0.1;
-       vecG += 0.2;
-       vecB += 0.3;
 
-       color = [colorR, colorG, colorB];
-   }, 1000)
+    fill("white");
+    textFont(font);
+    textSize(40);
+    textAlign(CENTER, CENTER);
+    const str = "Click Anywhere to Start";
+    const bounds = font.textBounds(str, 0, 0, 40);
+    const x = (windowWidth - bounds.w) / 2;
+    const y = (windowHeight - bounds.h) / 2;
+    console.log(x, y)
+    text(str, x, y);
 
 }
+
+function mouseClicked() {
+    if (!clicked) {
+        clicked = true;
+
+        mySound.setVolume(0.5);
+    
+        fft = new p5.FFT(0.8, WIDTH);
+
+
+        mySound.play();
+
+        setInterval(_ => {
+            const colorR = Math.floor((1 + Math.sin(vecR)) * 128);
+            const colorG = Math.floor((1 + Math.sin(vecG)) * 128);
+            const colorB = Math.floor((1 + Math.sin(vecB)) * 128);
+            
+            vecR += 0.1;
+            vecG += 0.2;
+            vecB += 0.3;
+
+            color = [colorR, colorG, colorB];
+        }, 1000)
+
+        
+    }
+  }
 
 function windowResized() {
     resizeCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-
-    if (!stop) {
-        background('black')
-        noFill()
-        refresh()
-    } else {
-        mySound.stop()
+    if (clicked) {
+        noFill();
+        refresh();
     }
-   
-    
 }
 
 function refresh() {
+    background('black')
+
     const analyze = fft.analyze().sort((a, b) => a < b ? 1 : -1).filter(a => a > 0);
     // console.log(analyze.length)
     const waveform = fft.waveform();
